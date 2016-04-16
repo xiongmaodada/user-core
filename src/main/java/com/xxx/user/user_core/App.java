819925e322c893b5fe7928b99943b7ac7edb2568
xiongmaodada <964408385@qq.com>
2016-04-16 15:01:47 +0800

    测试从eclipse中提交java代码

---
+++ src/main/java/com/xxx/user/user_core/App.java
@@ -0,0 +1,13 @@
+package com.xxx.user.user_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
